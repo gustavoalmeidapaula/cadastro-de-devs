c80c3966c8359d0c1632ce8ca37da0ac5f427944
Gustavo Almeida <159791843+gustavoalmeidapaula@users.noreply.github.com>
2024-05-06 16:59:10 -0300

    Update3 script.js

--- src/js/script.js
+++ src/js/script.js
@@ -91,12 +91,12 @@ form.addEventListener('submit', (ev) =>{
             for(var i = 0; i < number_input; i++){
                 
                 mensagem[i] = "\n* Nome da tecnologia: " + tecnologia_name[i].value +
-                "\n* Tempo de experiência: " + time_experience[i].value
+                "\nTempo de experiência: " + time_experience[i].value
             }
 
             alert("Cadastro concluído!" +
-                "\n=> Nome do desenvolvedor: " + name + 
-                "\n=>Tecnologias e Tempo de Experiência:" + mensagem
+                "\n-> Nome do desenvolvedor: " + name + 
+                "\n->Tecnologias e Tempo de Experiência:" + mensagem
                 )
         })
     })
