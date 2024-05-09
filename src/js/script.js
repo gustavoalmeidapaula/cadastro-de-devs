bf70acc63922217a58f204d2265f892f7860cce2
Gustavo Almeida <159791843+gustavoalmeidapaula@users.noreply.github.com>
2024-05-09 14:12:59 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -8,31 +8,32 @@ form.addEventListener('submit', (ev) =>{
 
     button.addEventListener('click', (ev) =>{
         ev.preventDefault()
+        
         number_input++
 
         const button_cadastrar = document.getElementById('cadastrar')
         const div = document.getElementById('agrupamento')
 
-        const bloco = document.createElement('div')
+        const div2 = document.createElement('div')
         const br = document.createElement('br')
         const br1 = document.createElement('br')
         const br2 = document.createElement('br')
         const br3 = document.createElement('br')
 
-        const label_Tecnologia = document.createElement('label')
-        label_Tecnologia.innerText = 'Nome da tecnologia'
-        label_Tecnologia.setAttribute('for','tecnologias') 
+        const label_tecnologia = document.createElement('label')
+        label_tecnologia.innerText = 'Nome da tecnologia'
+        label_tecnologia.setAttribute('for','tecnologias') 
 
-        const input_tecnologias = document.createElement('input')
-        input_tecnologias.type = 'text'
-        input_tecnologias.name = 'tecnologias'
-        input_tecnologias.id = 'tecnologias'
-        input_tecnologias.placeholder = 'Digite o nome da tecnologia'
-        input_tecnologias.className = 'input_text_tecnologias'
+        const input_tecnologia = document.createElement('input')
+        input_tecnologia.type = 'text'
+        input_tecnologia.name = 'tecnologias'
+        input_tecnologia.id = 'tecnologias'
+        input_tecnologia.placeholder = 'Digite o nome da tecnologia'
+        input_tecnologia.className = 'input_text_tecnologia'
 
-        const subTitulo = document.createElement('label')
-        subTitulo.innerText = 'Tempo de experiência:'
-        subTitulo.setAttribute('for','input_time' + number_input) 
+        const titulo2 = document.createElement('label')
+        titulo2.innerText = 'Tempo de experiência:'
+        titulo2.setAttribute('for','input_time' + number_input) 
 
         const input_time1 = document.createElement('input')
         input_time1.type = 'radio'
@@ -67,13 +68,13 @@ form.addEventListener('submit', (ev) =>{
         const remove = document.createElement('button')
         remove.innerText = 'Remover tecnologia'
 
-        bloco.append(label_Tecnologia,br,input_tecnologias,br1,subTitulo,br2,input_time1,label_time1,input_time2,label_time2,input_time3,label_time3,br3,remove)
-        div.appendChild(bloco)
+        div2.append(label_tecnologia,br,input_tecnologia,br1,titulo2,br2,input_time1,label_time1,input_time2,label_time2,input_time3,label_time3,br3,remove)
+        div.appendChild(div2)
         form.appendChild(div)
 
         remove.addEventListener('click', (ev) =>{
             ev.preventDefault()
-            div.removeChild(bloco)
+            div.removeChild(div2)
             number_input--;
         })
 
@@ -82,19 +83,20 @@ form.addEventListener('submit', (ev) =>{
             ev.preventDefault()
 
             const name = document.getElementById('name').value
-            const tecnologia_name = document.querySelectorAll("input[id='tecnologias']")
+            const name_tecnologia = document.querySelectorAll("input[id='tecnologias']")
             const time_experience = document.querySelectorAll("input[type='radio']:checked")
             const mensagem = [''];
 
-            console.log({name, tecnologia_name, time_experience})
+            console.log({name, name_tecnologia, time_experience})
 
             for(var i = 0; i < number_input; i++){
                 
-                mensagem[i] = "\n- Nome da tecnologia: " + tecnologia_name[i].value +
+                mensagem[i] = "\n- Nome da tecnologia: " + name_tecnologia[i].value +
                 "\n- Tempo de experiência: " + time_experience[i].value
             }
 
             alert("Cadastro concluído!" +
+                "\n Seja bem-vindo(a) dev." +
                 "\n> Nome do desenvolvedor: " + name + 
                 "\n> Tecnologias e Tempo de Experiência:" + mensagem
                 )
